28a00818e8e94a912eef32593fb82adbce680988
ADAM <test@gmail.com>
2025-02-19 13:16:20 +0300

    FORMA

---
+++ src/app/create-user-from/create-user-from.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+   selector: 'app-create-user-from',
+   templateUrl: './create-user-from.html',
+   styleUrl: './create-user-from.scss',
+   standalone: true,
+   })
+export class CreateUserFromComponent{
+   }
